c9e9929d66221a73bd790997781e92781bfe2439
topsyakins <topsyakins@gmail.com>
2022-06-24 18:27:24 +0100

    initial commit

---
+++ tp.js
@@ -0,0 +1,12 @@
+
+
+        var name = " Akinbola Temitope Sunday";
+        console.log(name);
+
+        var height = " height: 1.87m";
+        console.log(height);
+
+        var country = "Nigeria";
+        console.log(country)
+
+        alert("Name: Akinbola Temitope Sunday" + "\n" +"Height: 1.87m" + "\n" + "Country: Nigeria");
